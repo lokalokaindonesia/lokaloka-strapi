f2972d5920ae728e2622d31a34c11a98c81606f9
Devarista <kampungvector@gmail.com>
2021-07-06 18:08:54 +0700

    update

--- api/product-category/models/product-category.js
+++ api/product-category/models/product-category.js
@@ -4,12 +4,12 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.name) {
-        data.slug = slugify(data.name);
+        data.slug = slugify(data.name.toLowerCase());
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.name) {
-        data.slug = slugify(data.name);
+        data.slug = slugify(data.name.toLowerCase());
       }
     },
   },
