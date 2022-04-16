b21b9b1707b663fa43d3a614b33df5061b4fc01b
lokalokaindonesia <77106328+lokalokaindonesia@users.noreply.github.com>
2022-04-16 11:27:54 +0700

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'lokaloka.qvvan.mongodb.net'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'lokaloka'),
-        username: env('DATABASE_USERNAME', 'amdinlokaloka'),
-        password: env('DATABASE_PASSWORD', 'iChemistry01'),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', ''),
